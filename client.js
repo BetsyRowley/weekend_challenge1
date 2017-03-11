ec1f4cb48a71d0e30d6fe63b1fae939c6c0acc7c
Betsy Rowley <BRowley8@gmail.com>
2017-03-11 15:00:04 -0600

    Creates all profiles

--- client.js
+++ client.js
@@ -1,20 +1,29 @@
 $(document).ready(function() {
 
-//Adds name & shoutout to DOM
-
-  $("#singleImage").append("<img src=" + peopleArray[0].image + ">");
-  $(".studentText").append("<p id = 'name'>" + peopleArray[0].name + "</p>");
-  $(".studentText").append("<p id = 'shoutOut'>" + peopleArray[0].shoutout + "</p>");
-
-
+createProfiles();
 
 createGallery();
 
 });
 
 
-function createProfiles() {
+//Adds name & shoutout for each person
 
+function createProfiles() {
+  for( var i = 0; i < peopleArray.length; i++) {
+
+  $(".studentContainer").append("<div></div>");
+
+  var $el = $(".studentContainer").children().last();
+
+  $el.append("<img src=" + peopleArray[i].image + ">");
+  $el.append("<p id = 'name'>" + peopleArray[i].name + "</p>");
+  $el.append("<p id = 'shoutOut'>" + peopleArray[i].shoutout + "</p>");
+  }
+
+  // $("#singleImage").append("<img src=" + peopleArray[i].image + ">");
+  // $(".studentText").append("<p id = 'name'>" + peopleArray[i].name + "</p>");
+  // $(".studentText").append("<p id = 'shoutOut'>" + peopleArray[i].shoutout + "</p>");
 }
 
 
