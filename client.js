905859743c40aa88f877b8c9a58feade66e19a1b
Betsy Rowley <BRowley8@gmail.com>
2017-03-11 12:22:10 -0600

    Creates single profile

--- client.js
+++ client.js
@@ -1,1 +1,7 @@
-console.log(peopleArray);
+$(document).ready(function() {
+
+//Adds name & shoutout to DOM
+  $(".studentContainer").append("<p id = 'name'>" + peopleArray[0].name + "</p>");
+  $("#singleImage").append("<img src='Chi/Anisa Abdulkadir 2.jpg'>");
+  $(".studentContainer").append("<p id = 'shoutOut'>" + peopleArray[0].shoutout + "</p>");
+});
